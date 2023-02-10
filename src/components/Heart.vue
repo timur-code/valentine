26c096c161853da8f2fe74137c3168aa1bb2eca8
Timur Umurzakov <goust327@gmail.com>
2023-02-10 21:17:25 +0600

    added fixes

--- src/components/Heart.vue
+++ src/components/Heart.vue
@@ -10,8 +10,24 @@
             <button type="button" v-if="yesCount === 0" @click="yesCount++">Yes</button>
             <button type="button" v-if="noCount === 0 && yesCount === 0" @click="noCount++">No</button>
         </div>
-        <div class="flex justify-evenly">
-            <div class="text-xl whitespace-pre-wrap" :class="[yesCount > 0 ? 'text-red-400' : 'text-red-700']">{{ response }}</div>
+        <div class="flex justify-evenly flex-col" v-if="response.length > 0">
+            <div class="text-xl whitespace-pre-wrap" :class="[yesCount > 0 ? 'text-red-400' : 'text-red-700']">
+                {{ response }}
+            </div>
+            <div class="text-xl py-3">
+                Scroll down 
+            </div>
+            <div class="flex items-center flex-col w-auto">
+                <img class="object-cover rounded-lg shadow-lg lg:max-w-lg" src="../assets/1.jpg" alt="Love">
+                <p class="py-3">Thank you</p>
+                <img class="object-cover rounded-lg shadow-lg lg:max-w-lg" src="../assets/2.jpg" alt="Love">
+                <p class="py-3">for being</p>
+                <img class="object-cover rounded-lg shadow-lg lg:max-w-lg" src="../assets/3.jpg" alt="Love">
+                <p class="py-3">with me</p>
+                <img class="object-cover rounded-lg shadow-lg lg:max-w-lg" src="../assets/4.jpg" alt="Love">
+                <p class="py-3">I want to be with you always</p>
+                <img class="object-cover rounded-lg shadow-lg lg:max-w-lg" src="../assets/5.jpg" alt="Love">
+            </div>
         </div>
     </div>
 </template>
